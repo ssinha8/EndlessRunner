c8d27afc9c4d172adcc88920d5b1b806a4473023
nicoletteL <nantisde@ucsc.edu>
2022-04-23 17:11:49 -0700

    Added play scene. Play and Menu are bare bones.

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -1,1 +1,12 @@
 //TODO: add basic scene functionality
+class Menu extends Phaser.Scene {
+  constructor() {
+      super("menuScene");
+  }
+
+
+  update() {
+    // Right now this just plays the game scene
+    this.scene.start('playScene');
+  }
+}
---
+++ src/scenes/Play.js
@@ -0,0 +1,20 @@
+class Play extends Phaser.Scene {
+  constructor() {
+      super("playScene");
+  }
+
+  // Eventually load sprites and spritesheets (animation)
+  preload() {
+
+  }
+
+  // Does nothing right now
+  create() {
+
+  }
+  
+  // Does nothing right now
+  update() {
+
+  }
+}
