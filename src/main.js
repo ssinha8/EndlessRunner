26c60825ea0397f1f37773f965d96211a2135f24
AnnaSaysHi <headsilosetailsyouwin@gmail.com>
2022-05-04 16:29:48 -0700

    Added comment header to main.js

--- src/main.js
+++ src/main.js
@@ -1,3 +1,40 @@
+/*
+#############################################################################
+#                                                                           #
+#   COMMENTS                                                                #
+#                                                                           #
+#   Game name: Moonjump                                                     #
+#                                                                           #
+#   Completed on Wednesday, May 4 2022                                      #
+#                                                                           #
+#   Collaborators                                                           #
+#   Tracie Thornbury                                                        #
+#   Nicolette Antisdel                                                      #
+#   Anna Perlow                                                             #
+#   Shloak Sinha                                                            #
+#                                                                           #
+#   Creative tilt:                                                          #
+#                                                                           #
+#   The idea of an endless runner where you're forced to jump constantly    #
+#   and you can only affect the height of your jumps was decided on         #
+#   extremely early into this game's development. Unfortunately, most of    #
+#   our ideas (like collectible bunny capsules that would increase score    #
+#   and cause bunnies to appear in the background) had to be scrapped       #
+#   due to lack of time, and we only had time to implement this much.       #
+#   -Anna                                                                   #
+#                                                                           #
+#   The art style is a saturated 2-D style, with stark contrast between     #
+#   the background and the main character. The use of unsaturated colors    #
+#   for the bunny attracts your eyes to its position. However, when an      #
+#   obstacle appears, the use of a more saturated yellow or red is meant    #
+#   to attract the player's attention.                                      #
+#   This isn't a vector style, but maybe it would have been more            #
+#   appropriate, since the effects of the style are very similar.           #
+#   -Tracie                                                                 #
+#                                                                           #
+#############################################################################
+*/
+
 let config = {
     type: Phaser.AUTO,
     width: 960, //TODO: finalize width/height
@@ -8,7 +45,7 @@ let config = {
             debug: false
         }
     },
-    scene: [Menu, Shop, Play]
+    scene: [Menu, Shop, Play] // Shop scene has been scrapped due to lack of time.
 }
 
 let keyUP, keyDOWN, keyLEFT, keyRIGHT, keyCONFIRM, keyJUMP, keyCANCEL, keyPAUSE;
